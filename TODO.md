9e3d5be57ba9ca3d8e9173b7284d1e09de6b5faa
Austin Hathaway <ehathaway531@gmail.com>
2017-11-22 16:25:46 -0600

    add clear button

---
+++ TODO.md
@@ -0,0 +1,3 @@
+Add different color schemes to select
+Make the website prettier
+Change mouse focus so that it doesn't immediately change an element in the grid after it's reset.
--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 /**************************************
       generic helper functions
 ***************************************/
-function isInteger (num){
+function isInteger (num) {
   return !isNaN(num) && num % 1 === 0
 }
 
@@ -11,7 +11,7 @@ function getRandomIntInclusive(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
 }
 
-function generateRandomRGBVal(){
+function generateRandomRGBVal() {
   return getRandomIntInclusive(0, 255);
 }
 
@@ -26,16 +26,17 @@ function rgb (r, g, b){
 /**************************************
           app functions
 ***************************************/
-function createGridElement (width, height){
-    let newGridElement = document.createElement('div');
-    newGridElement.classList.add("gridElement");
-    newGridElement.style.width = width +'px';
-    newGridElement.style.height = height +'px';
-    newGridElement.style.float = 'left';
-    return newGridElement;
+function createGridElement (width, height) {
+  let newGridElement = document.createElement('div');
+  newGridElement.classList.add("gridElement");
+  newGridElement.classList.add("gridElementDefaultState");
+  newGridElement.style.width = width +'px';
+  newGridElement.style.height = height +'px';
+  newGridElement.style.float = 'left';
+  return newGridElement;
 }
 
-function changeGridElementColor (e){
+function changeGridElementColor (e) {
   let r = generateRandomRGBVal();
   let g = generateRandomRGBVal();
   let b = generateRandomRGBVal();
@@ -43,21 +44,86 @@ function changeGridElementColor (e){
   e.target.style.background = rgb(r,g,b);
 }
 
+function resetGrid () {
+  gameWindow.innerHTML = '';
+  for (var i = 0; i < numGridElementsPerRow; i++){
+    for(var j = 0; j < numGridElementsPerRow; j++){
+      let newGridElement = createGridElement(widthGridElement, heightGridElement)
+      newGridElement.addEventListener("mouseover", changeGridElementColor);
+      gameWindow.appendChild(newGridElement);
+    }
+  } 
+}
+
+function restart (e) {
+  //retrieve new num elements
+  let numElements = prompt("How many squares per side?", "4");
+
+  //validate entry
+  /*note:
+           I don't like rerunning the validation checks in this code
+           but I'd like to return an error message to the user.
+           If the validation checks are changed this won't fail;
+           it just won't return a specific message. 
+           I could possibly change the validation function to
+           return an error message
+  */
+  if (!validateNumGridElementsPerRow(numElements)) {
+    //determine the error
+    let errorMessage = "";
+    if (!isInteger(numElements)) 
+      errorMessage = " (Not an integer)";
+    else if (numElements < minGridElementsPerRow)
+      errorMessage = " (Number < " + minGridElementsPerRow + ")";
+    else if (numElements > maxGridElementsPerRow)
+      errorMessage = " (Number > " + maxGridElementsPerRow + ")";
+
+    alert("Invalid entry." + errorMessage + " Setting to default value");
+    numElements = defaultGridElementsPerRow;
+  }
+  setNumGridElementsPerRow(numElements);
+  resetGrid();
+}
+
+function setNumGridElementsPerRow (numElements) {
+  if (!validateNumGridElementsPerRow(numElements)) 
+    numElements = defaultGridElementsPerRow;
+  numGridElementsPerRow = numElements;
+  refreshGridNumDependentSettings();
+}
+
+function validateNumGridElementsPerRow (numElements) {
+  return isInteger(numElements) 
+      && numElements >= minGridElementsPerRow
+      && numElements <= maxGridElementsPerRow;
+}
+
+function refreshGridNumDependentSettings () {
+  //i had to subtract 1 in the below function to keep rounding errors from
+  //occasionally causing elements to spill over into the wrong row
+  widthGridElement = ((gameWindow.clientWidth - 1) / numGridElementsPerRow);
+  heightGridElement = widthGridElement;
+}
 /**************************************
               main
 ***************************************/
-//initialize variables
+//declare variables
 const gameWindow = document.querySelector('#gameWindow');
-let numGridElementsPerRow = 4;
-let widthGridElement = (gameWindow.clientWidth / numGridElementsPerRow);
-let heightGridElement = widthGridElement;
+const clearButton = document.querySelector('#clearButton');
+const maxGridElementsPerRow = 100;
+const minGridElementsPerRow = 1;
+const defaultGridElementsPerRow = 4;
+let numGridElementsPerRow = 0;
+let widthGridElement = 0;
+let heightGridElement = 0;
 
-//initialize the grid elements and add them to the game board
-for (var i = 0; i < numGridElementsPerRow; i++){
-  for(var j = 0; j < numGridElementsPerRow; j++){
-    let newGridElement = createGridElement(widthGridElement, heightGridElement)
-    newGridElement.addEventListener("mouseover", changeGridElementColor);
-    gameWindow.appendChild(newGridElement);
-  }
-}
+//initialize variables
+setNumGridElementsPerRow(4);
+
+//add event listeners
+clearButton.addEventListener("click", restart);
+
+//initialize the grid elements and add to gameWindow
+resetGrid();
+
 console.log(heightGridElement);
